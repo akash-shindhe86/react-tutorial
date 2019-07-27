f2cde1c7209b334f0aad07f143070aada951e7ce
akash.shindhe <Akash.Shinde@dunkinbrands.com>
2019-07-27 18:57:31 -0400

    Updates

---
+++ precache-manifest.2b28bd739220b6cec8f216d42584ffe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4086455a892df60affdbec6429f7c51",
+    "url": "/akash-shindhe86/react-tutorial/index.html"
+  },
+  {
+    "revision": "c82a84d7ed046932bf27",
+    "url": "/akash-shindhe86/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/akash-shindhe86/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "c82a84d7ed046932bf27",
+    "url": "/akash-shindhe86/react-tutorial/static/js/main.85cb9dae.chunk.js"
+  },
+  {
+    "revision": "e3c6cbe3f753bfefb60b",
+    "url": "/akash-shindhe86/react-tutorial/static/js/runtime~main.f2415b73.js"
+  }
+]);
